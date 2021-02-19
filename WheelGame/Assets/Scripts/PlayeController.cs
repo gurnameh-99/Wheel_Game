8869e2abf398ba1f71050778a144bdbd2e6ce1ea
gurnameh-99 <gurunameh.work@gmail.com>
2021-02-19 22:51:18 +0530

    basic structure added

---
+++ WheelGame/Assets/Scripts/PlayeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayeController : MonoBehaviour
+{
+    public CharacterController controller;
+    public float moveSpeed = 5f;
+    public float turnSpeed = 20f;
+    
+    private Vector3 direction = new Vector3(0f,0f,1f);
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.Move(direction * moveSpeed * Time.deltaTime);
+        transform.Rotate(new Vector3(100, 0, 0) * Time.deltaTime);
+    }
+}
